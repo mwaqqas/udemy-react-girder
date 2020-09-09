5308825b71418596ee5f534eda728945e3265179
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2020-09-09 21:20:02 +0300

    finished section 10

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+import ImageCard from './ImageCard';
+import './ImageList.css';
+
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return (
-      <img key={image.id} alt={image.description} src={image.urls.thumb} />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
 
   console.log(props.images);
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
