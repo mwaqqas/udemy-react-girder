90e0803846aff93c6c150cebdb40a53c1ebfe2fd
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2020-09-19 07:41:29 +0300

    In the middle of implementing fetch user logic

---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class UserHeader extends Component {
+  render() {
+    return <div>userheader</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return;
+};
+
+export default UserHeader;
